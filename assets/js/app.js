501f66404b9ac646e76aae6c52bd47e6f1f8772e
Tim Bernal <tbernal14@gsb.columbia.edu>
2015-05-09 20:41:22 -0400

    Fixing js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,16 @@ var main = function() {
   /* call single page nav */
   $('.learn-more-btn').singlePageNav;
 
-	$('singleSessionSignup').on('click', function() {
+	$('#singleSessionSignup').on('click', function() {
 	  ga('send', 'event', 'buyButton', 'click', 'singleSession', 35);
 	});
-	$('fivePackSignup').on('click', function() {
+	$('#fivePackSignup').on('click', function() {
 	  ga('send', 'event', 'buyButton', 'click', 'fivePack', 150);
 	});
-	$('customSignup').on('click', function() {
+	$('#customSignup').on('click', function() {
 	  ga('send', 'event', 'buyButton', 'click', 'customSession', 200);
 	});
-	$('formComplete').on('click', function() {
+	$('#formComplete').on('click', function() {
 	  ga('send', 'event', 'buyButton', 'click', 'fromComplete');
 	});
 };
