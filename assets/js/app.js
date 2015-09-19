d74a0ba45e29ba0cfbe1bf9f6269f974ccc6f307
Tim Bernal <tim.bernal@intentmedia.com>
2015-09-19 17:34:19 -0400

    for student page, swapped google form for parse and added mandrill

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,10 @@ var main = function() {
 	$('#ss-submit').on('click', function() {
 		ga('send', 'event', 'buyButton', 'click', 'formComplete');
 	});
+
+	$('#ss-submit-student').on('click', function() {
+		ga('send', 'event', 'signUpStudent', 'click', 'formComplete');
+	});
 	
 	$('#share_fb').on('click', function() {
 		ga('send', 'event', 'share', 'click', 'fb');
@@ -105,6 +109,77 @@ var main = function() {
 					window.location.href = "thank-you-prospective.html";
 				},
 				error: function(formObject, error) {
+					// form was not saved
+					console.log(error);	
+				}
+			});
+		};
+	});
+
+	$('#ss-form-student').submit(function(e){
+		console.log('Submit');
+		
+		Parse.initialize("lv8wq9RY8eZx0Hyevu8PaMVOTDHcH5JTYYpFsX6Z", "x9MWoecCxDBaBqp5FAc7W9kiLJFTHJg1vKY3DFfV");
+		var StudentObject = Parse.Object.extend("StudentObject");
+		var studentObject = new StudentObject();
+
+		/* Check required fields have been completed */
+		if ($('#firstName').val() == "") {
+			console.log('firstName was empty');
+			$('#firstName').focus();
+			$('#firstNameErrorMessage').html("<span>This is a required field</span>");
+			e.preventDefault();
+		} else if ($('#lastName').val() == "") {
+			console.log('lastName was empty');
+			$('#lastName').focus();
+			$('#lastNameErrorMessage').html("<span>This is a required field</span>");
+			e.preventDefault();
+		} else if ($('#email').val().indexOf('@') == -1) {
+			console.log('email did not have @ symbol in it');
+			$('#email').focus();
+			$('#emailErrorMessage').html("<span>This is a required field</span>");
+			e.preventDefault();
+		} else if ($('input[name=school]:checked').val().length == 0) {
+			console.log('No schools selected');
+			$('#school').focus();
+			$('#schoolErrorMessage').html("<span>This is a required field</span>");
+			e.preventDefault();
+		} else if ($('#gradYear option:selected').val() == "") {
+			console.log('No grad year entered');
+			$('#gradYearErrorMessage').html("<span>This is a required field</span>");
+			e.preventDefault();
+		} else {
+			e.preventDefault();
+			console.log('All fields were okay');
+
+			var firstName =  $('input[name=firstName]').val();
+			var lastName =  $('input[name=lastName]').val();
+			var email =  $('input[name=email]').val();
+			var gradYear = $('#gradYear option:selected').val()
+			var school = $('input[name=school]:checked').val();
+
+			studentObject.save({
+				firstName: firstName,
+				lastName: lastName,
+				email: email,
+				gradYear: gradYear,
+				school: school
+			}, {
+				success: function(studentObject) {
+					// form was saved successfully
+					// test cloud function
+					console.log("save successful");
+					Parse.Cloud.run('mailStudent', {firstName: firstName, email: email}, {
+						success: function(message) {
+							console.log("Cloud function success");
+						},
+						error: function(error) {
+							console.log("Cloud function error");
+						}
+					});
+					window.location.href = "thank-you-prospective.html";
+				},
+				error: function(studentObject, error) {
 					// form was not saved
 					console.log(error);	
 				}
